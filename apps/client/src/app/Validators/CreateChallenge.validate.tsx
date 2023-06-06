8d2bf235133a09ab4be8192fc91be8bf7001ef51
Khang <khang@e9.digital>
2023-06-06 16:29:25 +0700

    Validate fields contain only space

--- apps/client/src/app/Validators/CreateChallenge.validate.tsx
+++ apps/client/src/app/Validators/CreateChallenge.validate.tsx
@@ -6,18 +6,23 @@ export const CreateChallengeValidationSchema = () => {
   return yup.object().shape({
     goal: yup
       .string()
+      .trim()
       .required(t('new_challenge_screen.your_goal_required') as string),
 
     benefits: yup
       .string()
+      .trim()
       .required(t('new_challenge_screen.benefits_required') as string),
 
     reasons: yup
       .string()
+      .trim()
       .required(t('new_challenge_screen.reasons_required') as string),
 
     achievementTime: yup
       .string()
-      .required(t('new_challenge_screen.time_to_reach_goal_required') as string),
+      .required(
+        t('new_challenge_screen.time_to_reach_goal_required') as string
+      ),
   });
 };
--- apps/client/src/app/Validators/EditChallenge.validate.tsx
+++ apps/client/src/app/Validators/EditChallenge.validate.tsx
@@ -6,18 +6,23 @@ export const EditChallengeValidationSchema = () => {
   return yup.object().shape({
     goal: yup
       .string()
+      .trim()
       .required(t('edit_challenge_screen.your_goal_required') as string),
 
     benefits: yup
       .string()
+      .trim()
       .required(t('edit_challenge_screen.benefits_required') as string),
 
     reasons: yup
       .string()
+      .trim()
       .required(t('edit_challenge_screen.reasons_required') as string),
 
     achievementTime: yup
       .string()
-      .required(t('edit_challenge_screen.time_to_reach_goal_required') as string),
+      .required(
+        t('edit_challenge_screen.time_to_reach_goal_required') as string
+      ),
   });
 };

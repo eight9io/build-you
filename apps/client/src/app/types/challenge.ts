635fdf2b7fcefb3e195844e2a4e9ffb241075d6c
khang duong <khang@e9.digital>
2023-06-06 09:31:48 +0000

    Create new challenge modal + Edit challenge modal UI + Fix styling

---
+++ apps/client/src/app/types/challenge.ts
@@ -0,0 +1,14 @@
+export interface ICreateChallenge {
+  goal: string;
+  benefits: string;
+  reasons: string;
+  achievementTime: Date;
+  owner: string;
+}
+
+export interface IEditChallenge {
+  goal: string;
+  benefits: string;
+  reasons: string;
+  achievementTime: Date;
+}

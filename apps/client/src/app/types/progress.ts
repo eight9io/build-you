1d0effc21c90130806a3552f803d18e32f3bb298
Khang <khang@e9.digital>
2023-06-12 17:30:01 +0700

    Integrate get number of likes and comments API

--- apps/client/src/app/types/progress.ts
+++ apps/client/src/app/types/progress.ts
@@ -4,3 +4,23 @@ export interface ICreateProgress {
   caption: string;
   location: string;
 }
+
+export interface IProgressLike {
+  progress: string; // progress id
+  user: string; // user id
+}
+
+export interface ICreateProgressLike {
+  progress: string; // progress id
+}
+
+export interface IProgressComment {
+  progress: string; // progress id
+  user: string; // user id
+  comment: string;
+}
+
+export interface ICreateProgressComment {
+  progress: string; // progress id
+  comment: string;
+}

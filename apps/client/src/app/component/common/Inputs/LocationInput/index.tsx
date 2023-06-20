ae844527bd2c9c2300d635d2e8950febb3fe6733
khang duong <khang@e9.digital>
2023-06-20 07:02:49 +0000

    Location input when add new progress

--- apps/client/src/app/component/common/Inputs/LocationInput/index.tsx
+++ apps/client/src/app/component/common/Inputs/LocationInput/index.tsx
@@ -1,20 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
 import clsx from 'clsx';
 import { Controller } from 'react-hook-form';
-import { View, Text, TextInput } from 'react-native';
-
+import { View, Text, Linking } from 'react-native';
+import * as Location from 'expo-location';
 import { useTranslation } from 'react-i18next';
 
 import LocationSvg from '../assets/location.svg';
 import ErrorText from '../../ErrorText';
+import TextInput from '../TextInput';
+import ConfirmDialog from '../../Dialog/ConfirmDialog';
+import SelectPicker from '../../Pickers/SelectPicker';
+import { ISelectOption } from '../../../../types/common';
+import { getNearbyLocations } from '../../../../service/location';
+import { MOCK_ADDRESS_OPTIONS } from '../../../../mock-data/location';
 
 interface ILocationInputProps {
   control?: any;
+  setFormValue?: any;
   errors?: any;
+  onLocationPicked?: (location: string) => void;
 }
 
-const LocationInput: React.FC<ILocationInputProps> = ({ control, errors }) => {
+const LocationInput: React.FC<ILocationInputProps> = ({
+  control,
+  setFormValue,
+  errors,
+}) => {
   const { t } = useTranslation();
+  const [requireLocationModalVisible, setRequireLocationModalVisible] =
+    useState(false);
+  const [showLocationPicker, setShowLocationPicker] = useState(false);
+  const [selectedLocationIndex, setSelectedLocationIndex] = useState<
+    number | undefined
+  >();
+  const [nearbyLocations, setNearbyLocations] = useState<ISelectOption[]>([]);
+  const [nextPageToken, setNextPageToken] = useState<string | undefined>(); // for load more
+
+  const handleTextInputPress = async () => {
+    const currentPermission = await Location.getForegroundPermissionsAsync();
+    if (currentPermission && currentPermission.granted) {
+      await fetchNearbyLocations();
+      setShowLocationPicker(true);
+    } else {
+      // Request permission to access location
+      const permission = await Location.requestForegroundPermissionsAsync();
+      if (permission.granted) {
+        await fetchNearbyLocations();
+        setShowLocationPicker(true);
+      } else if (!permission.canAskAgain) {
+        // Case: user denied permission => show dialog to open settings to enable location
+        setRequireLocationModalVisible(true);
+      }
+    }
+    
+  };
+
+  const fetchNearbyLocations = async (isLoadMore?: boolean) => {
+    const { coords } = await Location.getCurrentPositionAsync();
+    const extractedCoords = `${coords.latitude},${coords.longitude}`;
+    if (isLoadMore !== undefined && isLoadMore) {
+      if (!nextPageToken) return;
+      const { nextPageToken: token, addresses } = await getNearbyLocations(
+        extractedCoords,
+        nextPageToken
+      );
+      if (addresses.length > 0)
+        setNearbyLocations([...nearbyLocations, ...addresses]);
+      setNextPageToken(token);
+    } else {
+      const { nextPageToken: token, addresses } = await getNearbyLocations(
+        extractedCoords
+      );
+      if (addresses.length > 0) setNearbyLocations(addresses);
+      setNextPageToken(token);
+    }
+  };
+  const handleCloseRequireLocationModal = () => {
+    setRequireLocationModalVisible(false);
+  };
+  const handleConfirmRequireLocationModal = () => {
+    setRequireLocationModalVisible(false);
+    Linking.openSettings();
+  };
+
+  const handleLocationPicked = (index: number) => {
+    if (index >= 0) {
+      setSelectedLocationIndex(index);
+      setFormValue('location', nearbyLocations[index].label);
+    }
+    setShowLocationPicker(false);
+  };
   return (
     <View>
       <Controller
@@ -23,37 +98,40 @@ const LocationInput: React.FC<ILocationInputProps> = ({ control, errors }) => {
           required: true,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
-          <View className={clsx('flex flex-col gap-1')}>
-            <Text
-              className={clsx('text-primary-default text-sm font-semibold')}
-            >
-              {t('new_challenge_screen.location')}
-            </Text>
-
-            <View
-              className={clsx(
-                'border-gray-medium bg-gray-veryLight relative flex h-12 w-full flex-row rounded-[10px] border-[1px] px-3 py-2 text-base font-normal'
-              )}
-            >
-              <TextInput
-                placeholder={
-                  t('new_challenge_screen.enter_your_location') as string
-                }
-                placeholderTextColor={'#C5C8D2'}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                multiline
-              />
-              <View className="absolute right-3 top-3 pl-4">
-                <LocationSvg />
-              </View>
-            </View>
-          </View>
+          <TextInput
+            placeholder={t('new_progress_modal.enter_your_location') as string}
+            label={t('new_progress_modal.location') as string}
+            placeholderTextColor={'#C5C8D2'}
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            onPress={handleTextInputPress}
+            rightIcon={<LocationSvg />}
+          />
         )}
         name="location"
       />
       {errors ? <ErrorText message={errors.message} /> : null}
+      <SelectPicker
+        title={t('new_progress_modal.enter_your_location') || ''}
+        show={showLocationPicker}
+        data={nearbyLocations}
+        selectedIndex={selectedLocationIndex}
+        onSelect={handleLocationPicked}
+        onCancel={() => {
+          setShowLocationPicker(false);
+        }}
+        onLoadMore={() => fetchNearbyLocations(true)}
+      />
+      <ConfirmDialog
+        title={t('dialog.alert_title') || ''}
+        description={t('new_progress_modal.enable_location_required') || ''}
+        isVisible={requireLocationModalVisible}
+        onClosed={handleCloseRequireLocationModal}
+        closeButtonLabel={t('close') || ''}
+        confirmButtonLabel={t('dialog.open_settings') || ''}
+        onConfirm={handleConfirmRequireLocationModal}
+      />
     </View>
   );
 };
--- apps/client/src/app/component/common/Pickers/SelectPicker/index.tsx
+++ apps/client/src/app/component/common/Pickers/SelectPicker/index.tsx
@@ -11,20 +11,25 @@ import {
   ScrollView,
   NativeViewGestureHandler,
 } from 'react-native-gesture-handler';
+import BottomSheetOption from '../../Buttons/BottomSheetOption';
 
 interface ISelectPickerProps {
   show: boolean;
+  title: string;
   data: Array<any> | null | undefined;
   selectedIndex?: number;
   onSelect: (index: number) => void;
   onCancel: () => void;
+  onLoadMore?: () => void;
 }
 const SelectPicker: FC<ISelectPickerProps> = ({
   show,
+  title,
   data,
   selectedIndex,
   onSelect,
   onCancel,
+  onLoadMore,
 }) => {
   const [selected, setSelected] = useState<number>(0);
   // const flatListRef = useRef<FlatList>(null);
@@ -50,21 +55,21 @@ const SelectPicker: FC<ISelectPickerProps> = ({
       style={{ margin: 0, justifyContent: 'flex-end' }}
     >
       <TouchableOpacity
-        style={{ height: '30%', backgroundColor: 'transperent' }}
+        style={{ height: '30%', backgroundColor: 'transparent' }}
         activeOpacity={0}
         onPressOut={onCancel}
       ></TouchableOpacity>
-      <View className="flex-1">
+      <View className='flex-1'>
         <BottomSheet2 onClose={onCancel} snapPoints={['100%']}>
           <View className="relative">
             <View className="flex w-full flex-row items-center justify-center pb-8">
-              <Text className="text-base font-semibold">Occupation</Text>
+              <Text className="text-base font-semibold">{title}</Text>
             </View>
-            <ScrollView className="h-4/5">
+            {/* <ScrollView className="h-4/5 bg-red-400">
               <View className="flex h-full w-full flex-col pb-10 ">
                 {data?.map((item, index) => {
                   return (
-                    <View className="px-4" key={`${item?.label + index}`}>
+                    <View className="px-4" key={index}>
                       <Button
                         onPress={() => setSelected(index)}
                         title={item.label}
@@ -81,32 +86,36 @@ const SelectPicker: FC<ISelectPickerProps> = ({
                   );
                 })}
               </View>
-            </ScrollView>
+            </ScrollView> */}
 
-            {/* TODO: if we want to use FlatList instead of ScrollView for scrolling to the selected item, we need to fix the height of the FlatList */}
-            {/* <FlatList
+            <FlatList
               data={data}
-              keyExtractor={(item, index) => `${item?.label + index}`}
+              initialNumToRender={data?.length || 0}
+              keyExtractor={(item, index) =>
+                `${item.key}-${item.label}-${index}`
+              }
               renderItem={({ item, index }) => {
                 return (
-                  <View className="px-4" key={`${item?.label + index}`}>
-                    <Button
-                      onPress={() => onSelect(index)}
+                  <View className="mb-2 px-4">
+                    <BottomSheetOption
+                      onPress={() => setSelected(index)}
                       title={item.label}
                       containerClassName={clsx(
                         'focus:bg-gray-light',
-                        index === selectedIndex && 'bg-gray-light'
+                        index === selected && 'bg-gray-light'
                       )}
                       textClassName={clsx(
                         'text-base font-normal',
-                        index === selectedIndex && 'font-semibold'
+                        index === selected && 'font-semibold'
                       )}
                     />
                   </View>
                 );
-              }
-              }
-            /> */}
+              }}
+              onEndReached={onLoadMore}
+              onEndReachedThreshold={0.5}
+              className="h-4/5"
+            />
             <View className="absolute bottom-[-40px] h-12 w-full px-4">
               <Button
                 title={'Save'}

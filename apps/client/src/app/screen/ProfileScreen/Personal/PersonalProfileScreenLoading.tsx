526f7e54a916fcf7da5a072d0dd2141f7e1cea75
Nerdy Mike <michaelle.dev@gmail.com>
2023-06-28 11:00:32 +0700

    fix 28/6 hoang

--- apps/client/src/app/screen/ProfileScreen/Personal/PersonalProfileScreenLoading.tsx
+++ apps/client/src/app/screen/ProfileScreen/Personal/PersonalProfileScreenLoading.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
+
+import BuildYouLogo from '../../../common/svg/buildYou_logo.svg';
+import StarLogo from '../../../common/svg/auto_awesome.svg';
 
 const PersonalProfileScreenLoading = () => {
-  // TODO: add skeleton loading
+  const { t } = useTranslation();
   return (
-    <SafeAreaView className="justify-content: space-between flex-1 bg-white pt-6">
-      <View className="flex flex-row items-center justify-center px-6">
-        <Text>Loading...</Text>
+    <SafeAreaView className="mt-28 flex h-full flex-col items-center">
+      <View className="">
+        <BuildYouLogo />
+      </View>
+      <View className="flex flex-col items-center justify-center pt-32">
+        <StarLogo />
+
+        <View>
+          <Text className="text-gray-dark text-md mx-12 pt-4 text-center font-normal">
+            Welcome
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );

74502dab0f893febd5db236492d8e9adaa74a39b
Khang <khang@e9.digital>
2023-07-10 17:44:00 +0700

    Integrated update push token api

--- apps/client/src/app/common/enum.ts
+++ apps/client/src/app/common/enum.ts
@@ -4,3 +4,13 @@ export enum NOTIFICATION_TYPES {
   NEW_MENTION = 'new_mention',
   NEW_FOLLOWER = 'new_follower',
 };
+
+export enum NOTIFICATION_TOKEN_STATUS {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+};
+
+export enum NOTIFICATION_TOKEN_DEVICE_TYPE {
+  ANDROID = 'Android',
+  IOS = 'iOS',
+};

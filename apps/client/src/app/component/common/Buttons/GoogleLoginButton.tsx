28006c2b5979be5838b38b40dac42d1160298064
Khang <khang@e9.digital>
2023-07-22 09:31:04 +0700

    Refactor to use firebase with social login + Integrate API

--- apps/client/src/app/component/common/Buttons/GoogleLoginButton.tsx
+++ apps/client/src/app/component/common/Buttons/GoogleLoginButton.tsx
@@ -2,11 +2,19 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { FC, useEffect } from 'react';
 import Button from './Button';
 import IconGoogle from './asset/Google.svg';
+import { googleLogin } from '../../../service/auth';
+import GlobalDialogController from '../Dialog/GlobalDialogController';
+import { useAuthStore } from '../../../store/auth-store';
+import { addAuthTokensLocalOnLogin } from '../../../utils/checkAuth';
+import { useTranslation } from 'react-i18next';
 
 interface IGoogleLoginButtonProps {
   title?: string;
 }
 const GoogleLoginButton: FC<IGoogleLoginButtonProps> = ({ title }) => {
+  const { setAccessToken, setRefreshToken } = useAuthStore();
+  const { t } = useTranslation();
+
   useEffect(() => {
     GoogleSignin.configure();
   }, []);
@@ -14,8 +22,25 @@ const GoogleLoginButton: FC<IGoogleLoginButtonProps> = ({ title }) => {
   const handleGoogleBtnClicked = async () => {
     try {
       const userInfo = await GoogleSignin.signIn();
-      alert(userInfo.idToken);
+      if (userInfo.idToken) {
+        const res = await googleLogin(userInfo.idToken);
+        if (res.status === 201 || res.status === 200) {
+          setAccessToken(res?.data.authorization || null);
+          setRefreshToken(res?.data.refresh || null);
+          addAuthTokensLocalOnLogin(
+            res?.data.authorization || null,
+            res?.data.refresh || null
+          );
+        }
+      }
     } catch (error) {
+      GlobalDialogController.showModal({
+        title: 'Error',
+        message:
+          t('errorMessage:500') ||
+          'Something went wrong. Please try again later!',
+        button: 'OK',
+      });
       console.error(error);
     }
   };

caaed9e7520a3503eaff5c75d0c4a595afc64d83
Pham Thao <thao@e9.digital>
2023-08-02 11:54:26 +0700

    done setting

--- src/app/service/settings.ts
+++ src/app/service/settings.ts
@@ -3,3 +3,5 @@ import http from "../utils/http";
 
 export const serviceGetTerms = () =>
     http.get('/document/terms');
+export const serviceGetPrivacy = () =>
+    http.get('/document/privacy');

79ed23cd8d9bb9797515d4833ee9349644af4265
Khang <khang@e9.digital>
2023-08-16 15:57:27 +0700

    Add missing testID

--- src/app/screen/IntroScreen/IntroScreen.tsx
+++ src/app/screen/IntroScreen/IntroScreen.tsx
@@ -18,7 +18,7 @@ export const IntroScreen = ({ navigation, route }: any) => {
   }, []);
 
   return (
-    <View className="justify-content: space-between flex-1" testID="">
+    <View className="justify-content: space-between flex-1" testID="intro_screen">
       <View className="flex-1">
         <Image
           className="z-10 h-[100%] w-[100%] rounded-xl"

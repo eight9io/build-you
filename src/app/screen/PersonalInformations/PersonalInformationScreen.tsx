f114ab037adc20750da5cef33cb1b47e32e0d4c0
Nerdy Mike <michaelle.dev@gmail.com>
2023-09-08 16:22:28 +0700

    fix remove push noti token on logout: to be picked to develop

--- src/app/screen/PersonalInformations/PersonalInformationScreen.tsx
+++ src/app/screen/PersonalInformations/PersonalInformationScreen.tsx
@@ -19,6 +19,7 @@ import appleAuth from "@invertase/react-native-apple-authentication";
 import { serviceDeleteAccount } from "../../service/profile";
 import { CommonActions } from "@react-navigation/native";
 import LinkedInModal from "../../component/modal/LinkedInModal";
+import Spinner from "react-native-loading-spinner-overlay";
 
 const getGoogleToken = async () => {
   const { idToken } = await GoogleSignin.signIn();
@@ -114,14 +115,13 @@ export default function PersonalInformationScreen({ navigation }: any) {
   };
 
   const handleLogOut = () => {
-    logout();
+    logout(userData.id);
     navigation.dispatch(
       CommonActions.reset({
         index: 0,
         routes: [{ name: "IntroScreen" }],
       })
     );
-    logout();
     userProfileStoreOnLogout();
   };
 
@@ -160,6 +160,7 @@ export default function PersonalInformationScreen({ navigation }: any) {
       />
 
       <ScrollView>
+        {isLoading && <Spinner />}
         <View className={clsx("px-4 py-4")}>
           <Text className={clsx("text-h4 font-medium")}>
             {t("personal_information.description")}

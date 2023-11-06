5f1e29f3b104c31aaa8e42f9c896b7830be3cc56
Nerdy Mike <mike.thenerdy@gmail.com>
2023-11-06 17:41:53 +0700

    feat change to use reverse geolocation

--- src/app/service/location.ts
+++ src/app/service/location.ts
@@ -1,34 +1,24 @@
 import httpInstance from "../utils/http";
 import { GOOGLE_MAP_API } from "../common/constants";
-import { extractNearbyAddresses } from "../utils/googleMap.util";
+import { ExtractedGeoLocation, extractLocation } from "../utils/googleMap.util";
 import { ISelectOption } from "../types/common";
 import axios from "axios";
 import { CrashlyticService } from "./crashlytic";
 
 export const getNearbyLocations = async (
-  coords: string,
-  nextPageToken?: string
-): Promise<{
-  nextPageToken?: string;
-  addresses: ISelectOption[];
-}> => {
+  coords: string
+): Promise<ISelectOption[]> => {
   const params = new URLSearchParams();
   if (!GOOGLE_MAP_API.API_KEY) {
     throw new Error("Google Map API key is not defined");
   }
   params.append("key", GOOGLE_MAP_API.API_KEY || "");
-  if (nextPageToken) {
-    // A token that can be used to return up to 20 additional results. A next_page_token will not be returned if there are no additional results to display.
-    params.append("pagetoken", nextPageToken);
-  } else {
-    // The latitude/longitude around which to retrieve place information. Format: latitude,longitude
-    params.append("location", coords);
-    // Radius in meters, showing results within this distance from the specified location.
-    params.append("radius", GOOGLE_MAP_API.DEFAULT_RADIUS.toString());
-  }
+
   try {
     const response = await axios.get(
-      GOOGLE_MAP_API.NEARBY_SEARCH_ENDPOINT + "?" + params.toString(),
+      GOOGLE_MAP_API.NEARBY_SEARCH_ENDPOINT +
+        "?" +
+        `latlng=${coords}&key=${GOOGLE_MAP_API.API_KEY}`,
       {
         baseURL: GOOGLE_MAP_API.BASE_URL,
         headers: {
@@ -38,7 +28,7 @@ export const getNearbyLocations = async (
         },
       }
     );
-    return extractNearbyAddresses(response.data);
+    return extractLocation(response.data);
   } catch (error) {
     console.error("error: ", error);
     CrashlyticService({
--- src/app/utils/googleMap.util.ts
+++ src/app/utils/googleMap.util.ts
@@ -18,6 +18,23 @@ type ExtractedData = {
   addresses: ISelectOption[];
 };
 
+type GeoLocation = {
+  address_components: string[];
+  formatted_address: string;
+  geometry: {
+    location: {
+      lat: number;
+      lng: number;
+    };
+  };
+  place_id: string;
+  types: string[];
+};
+
+export type ExtractedGeoLocation = {
+  addresses: ISelectOption[];
+};
+
 export const extractNearbyAddresses = (data: {
   results: Location[];
   next_page_token: string;
@@ -33,4 +50,23 @@ export const extractNearbyAddresses = (data: {
     nextPageToken: next_page_token,
     addresses,
   };
+};
+
+export const extractLocation = (data: {
+  plus_code: {
+    compound_code: string;
+    global_code: string;
+  };
+  results: GeoLocation[];
+}) => {
+  // return list of locations in result
+  const { results } = data;
+  const addresses: ISelectOption[] = results.map((location) => {
+    return {
+      key: `${location.geometry.location.lat},${location.geometry.location.lng}`,
+      label: location.formatted_address,
+    };
+  });
+
+  return addresses;
 };

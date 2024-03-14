66614ee95f8c3681fef93ca93afc4014b999b58a
Khang <khang@lab.e9.digital>
2024-03-14 14:43:55 +0700

    Disable create certified challenge action until payment is ready

--- src/app/screen/ChallengesScreen/CreateChallengeScreenMain.tsx
+++ src/app/screen/ChallengesScreen/CreateChallengeScreenMain.tsx
@@ -120,25 +120,26 @@ const CreateChallengeScreenMain = () => {
     else navigation.navigate("CreateChallengeScreen");
   };
   const handleCreateCretifiedChallenge = async () => {
-    try {
-      const { data: isUserHasInDraftChallenge } =
-        await serviceGetIsUserHasInDraftChallenge();
-      if (isUserHasInDraftChallenge) {
-        GlobalDialogController.showModal({
-          title: t("dialog.in_draft.title"),
-          message: t("dialog.in_draft.description"),
-        });
-        return;
-      }
-    } catch (error) {
-      console.error("get is user has in draft challenge error", error);
-    }
-    // navigation.navigate("CartScreen");
-    navigation.navigate(
-      isCompany
-        ? "CreateCertifiedCompanyChallengeScreen"
-        : "CreateCertifiedChallengeScreen"
-    );
+    // Disable action until api is ready
+    // try {
+    //   const { data: isUserHasInDraftChallenge } =
+    //     await serviceGetIsUserHasInDraftChallenge();
+    //   if (isUserHasInDraftChallenge) {
+    //     GlobalDialogController.showModal({
+    //       title: t("dialog.in_draft.title"),
+    //       message: t("dialog.in_draft.description"),
+    //     });
+    //     return;
+    //   }
+    // } catch (error) {
+    //   console.error("get is user has in draft challenge error", error);
+    // }
+    // // navigation.navigate("CartScreen");
+    // navigation.navigate(
+    //   isCompany
+    //     ? "CreateCertifiedCompanyChallengeScreen"
+    //     : "CreateCertifiedChallengeScreen"
+    // );
   };
 
   useLayoutEffect(() => {
